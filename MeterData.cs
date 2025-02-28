087b582795ef29d31d8f5adb4f2b9342d743c14a
Vaibhav Singhal <vaibhav.singhal@kimbal.io>
2025-02-28 14:21:35 +0530

    addition of data

---
+++ MeterData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meter_screening_application
+{
+    internal class MeterData
+    {
+    }
+}
